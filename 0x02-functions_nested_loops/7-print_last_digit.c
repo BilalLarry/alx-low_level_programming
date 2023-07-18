b8a513dc12b8302831b93106902eb6152e6b0fb1
BilalLarry <bilallarry4@gmail.com>
2023-07-18 13:46:15 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -15,22 +15,5 @@ int print_last_digit(int n)
 		last_digit *= -1;
 
 	putchar(last_digit + '0');
-	return last_digit;
+	return (last_digit);
 }
-
-/**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int num = -12345;
-	int last_digit_value;
-
-	last_digit_value = print_last_digit(num);
-	putchar('\n');
-
-	return (0);
-}
-
