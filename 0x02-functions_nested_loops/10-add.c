236990e095c1ff6cf27abc5325feea8f77451cd4
BilalLarry <bilallarry4@gmail.com>
2023-07-18 14:59:07 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -10,21 +10,5 @@
  */
 int add(int a, int b)
 {
-	return a + b;
+	(return a + b);
 }
-
-/**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int num1 = 10, num2 = 20, sum;
-
-	sum = add(num1, num2);
-	printf("The sum of %d and %d is: %d\n", num1, num2, sum);
-
-	return (0);
-}
-
