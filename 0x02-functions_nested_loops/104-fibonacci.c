8a10dfa2b6f2ef9e7b5c66700b2f05b2aceac497
BilalLarry <bilallarry4@gmail.com>
2023-07-18 04:20:26 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	unsigned long int first = 1, second = 2, temp;
+	int count, max_count = 98;
+
+	printf("%lu, %lu", first, second);
+
+	for (count = 2; count < max_count; count++)
+	{
+		temp = first + second;
+		printf(", %lu", temp);
+
+		first = second;
+		second = temp;
+	}
+
+	printf("\n");
+	return (0);
+}
+
