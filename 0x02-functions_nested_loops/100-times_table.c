15a4d65f832a6c4eae3df8c407a10eb5a7700069
BilalLarry <bilallarry4@gmail.com>
2023-07-18 19:50:43 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <unistd.h>
+
 /**
  * print_times_table - prints the n times table, starting with 0
  * @n: The number for which to print the times table (0 <= n <= 15)
@@ -8,48 +8,40 @@ void print_times_table(int n)
 {
 	int row, column, product;
 
-	if (n < 0 || n > 15)
-		return;
+	if (n >= 0 && n <= 15)
+	{
 
 	for (row = 0; row <= n; row++)
 	{
-		for (column = 0; column <= n; column++)
+	_putchar('0');
+		for (column = 1; column <= n; column++)
 		{
+		_putchar(',');
+		_putchar(' ');
 			product = row * column;
 
-			if (column != 0)
-			{
-				_putchar(',');
+			if (column <= 99)
+			
 				_putchar(' ');
+				
 
-				if (product < 10)
-					_putchar(' ');
-			}
-
-			if (product < 100)
-				_putchar(' ');
+				if (product <= 9)
+			_putchar(' ');
 
 			if (product >= 100)
-				_putchar(product / 100 + '0');
+			{
+				_putchar((product / 100) + '0');
+_putchar((product / 10) % 10 + '0');
+			}
+			else if (product <= 99 && product >= 10)
+			{
+				_putchar((product / 10) + '0');
+			}
+				_putchar((product % 10) + '0');
 
-			if (product >= 10)
-				_putchar((product / 10) % 10 + '0');
-
-			_putchar(product % 10 + '0');
-		}
+	}
 		_putchar('\n');
 	}
 }
 
-/**
- * _putchar - writes a character to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return write(1, &c, 1);
-}
 
