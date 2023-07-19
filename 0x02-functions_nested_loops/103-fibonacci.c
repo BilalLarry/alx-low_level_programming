69b730ab6353334c38c74912dbab8d9228e6b181
BilalLarry <bilallarry4@gmail.com>
2023-07-19 02:23:11 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-int fibonacci_sum_even(int limit) {
+int fibonacci_sum_even(int limit)
+{
     int a = 1, b = 2, temp, sum_even = 0;
 
-    while (a <= limit) {
-        if (a % 2 == 0) {
+    while (a <= limit)
+    {
+        if (a % 2 == 0)
+        {
             sum_even += a;
         }
         temp = a;
@@ -15,7 +18,8 @@ int fibonacci_sum_even(int limit) {
     return sum_even;
 }
 
-int main() {
+int main(void)
+{
     int limit = 4000000;
     int result = fibonacci_sum_even(limit);
     printf("%d\n", result);
