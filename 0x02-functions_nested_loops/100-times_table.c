ded8cff11962b8e6a2d06ffcf1bd189aa491e342
BilalLarry <bilallarry4@gmail.com>
2023-07-19 03:15:14 -0700

    f

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,44 +1,27 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: The number for which to print the times table (0 <= n <= 15)
+ * main - Entry point
+ * Return: Always 0.
  */
-void print_times_table(int n)
+int fibonacci_sum_even(int limit)
 {
-    int row, column, product;
-
-    if (n >= 0 && n <= 15)
-    {
-        for (row = 0; row <= n; row++)
-        {
-            _putchar('0');
-            for (column = 1; column <= n; column++)
-            {
-                _putchar(',');
-                _putchar(' ');
-
-                product = row * column;
-
-                if (column <= 9)
-                    _putchar(' ');
-                else if (product <= 99)
-                    _putchar(' ');
-
-                if (product >= 100)
-                {
-                    _putchar((product / 100) + '0');
-                    _putchar(((product / 10)) % 10 + '0');
-                }
-                else if (product <= 99 && product >= 10)
-                {
-                    _putchar((product / 10) + '0');
-                }
-
-                _putchar((product % 10) + '0');
-            }
-            _putchar('\n');
-        }
-    }
+int a = 1, b = 2, temp, sum_even = 0;
+while (a <= limit)
+{
+if (a % 2 == 0)
+{
+sum_even += a;
 }
-
+temp = a;
+a = b;
+b += temp;
+}
+return (sum_even);
+}
+int main(void)
+{
+int limit = 4000000;
+int result = fibonacci_sum_even(limit);
+printf("%d\n", result);
+return (0);
+}
--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * Return: Always 0.
+ */
 int fibonacci_sum_even(int limit)
 {
-    int a = 1, b = 2, temp, sum_even = 0;
-
-    while (a <= limit)
-    {
-        if (a % 2 == 0)
-        {
-            sum_even += a;
-        }
-        temp = a;
-        a = b;
-        b += temp;
-    }
-
-    return sum_even;
+int a = 1, b = 2, temp, sum_even = 0;
+while (a <= limit)
+{
+if (a % 2 == 0)
+{
+sum_even += a;
 }
-
+temp = a;
+a = b;
+b += temp;
+}
+return sum_even;
+}
 int main(void)
 {
-    int limit = 4000000;
-    int result = fibonacci_sum_even(limit);
-    printf("%d\n", result);
-
-    return (0);
+int limit = 4000000;
+int result = fibonacci_sum_even(limit);
+printf("%d\n", result);
+return (0);
 }
 
