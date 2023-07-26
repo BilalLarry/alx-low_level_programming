83520a168f9e8b647680dc8622af65cbce6908fd
BilalLarry <bilallarry4@gmail.com>
2023-07-26 09:33:40 -0700

    ./0-strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,40 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-char *concatenateStrings(char *dest, const char *src)
+
+/**
+ * _strcat - Concatenates two strings.
+ * @dest: Pointer to the destination string.
+ * @src: Pointer to the source string.
+ *
+ * This function appends the src string to the dest string,
+ * overwriting the terminating null byte ('\0') at the end of dest,
+ * and then adds a terminating null byte.
+ *
+ * Return: Pointer to the resulting string dest.
+ */
+char *_strcat(char *dest,  char *src)
 {
-if (dest == NULL || src == NULL)
-{
-return NULL; // Handle invalid input pointers
-}
-	char *result = dest;
-// Move the dest pointer to the end of the destination string
-while (*dest != '\0')
-{
-dest++;
-}
-// Copy characters from src to dest until the end of src is reached
-	while (*src != '\0') 
-{
-	*dest = *src;
-	dest++;
-	src++;
-}
-// Add the terminating null byte to the concatenated string
-*dest = '\0';
-	return result;
-}
-int main()
-{
-char str1[50] = "Hello, ";
-    const char *str2 = "world!"
-// Concatenate str2 to str1 and store the result in str1
-	char *result = concatenateStrings(str1, str2);
-	if (result != NULL) {
-	printf("Concatenated string: %s\n", result);
-	} else {
-	printf("Error: Invalid input pointers.\n");
-	}
-	return 0;
+    char *result = dest;
+
+    while (*dest != '\0')
+    {
+        dest++;
+    }
+
+    while (*src != '\0')
+    {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+
+    *dest = '\0';
+
+    return result;
 }
 
