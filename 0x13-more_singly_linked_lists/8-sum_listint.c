1c0cef0c752e778b8d3783c70b67501fff191df2
BilalLarry <bilallarry4@gmail.com>
2023-08-29 13:54:26 -0700

    K

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -4,14 +4,14 @@
  * @head: first node
  * Return: sum
  */
-int sum_listint(listint_t*head)
-{       
-int sum =0;
+int sum_listint(listint_t *head)
+{
+int sum = 0;
 listint_t *temp = head;
 while (temp)
-{       
+{
 sum += temp->n;
 temp = temp->next;
 }
 return (sum);
-}   
+}
